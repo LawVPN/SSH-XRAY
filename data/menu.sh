0c05d2d81da9d8b472942a7456eeea0a7861dd40
Lukman Priya <129483274+LawVPN@users.noreply.github.com>
2023-06-07 17:11:55 +0700

    Update menu.sh

--- data/menu.sh
+++ data/menu.sh
@@ -182,12 +182,12 @@ myver="$(cat /opt/.ver)"
 
 if [[ $serverV > $myver ]]; then
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1 $NC ${WH}[${COLOR1}100${WH}]${NC} ${COLOR1}• ${RED}UPDATE AUTOSCRIPT TO THE NEWEST v$serverV${NC} " 
+echo -e "$COLOR1 $NC ${WH}[${COLOR1}100${WH}]${NC} ${COLOR1}• ${RED}UPDATE AUTOSCRIPT TO THE NEWEST ${WH}v$serverV${NC} " 
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-ltsver="[Update Available]"
+ltsver="[ Update Available ]"
 up2u="updatews"
 else
-ltsver="Latest Version"
+ltsver="[ Latest Version ]"
 up2u="menu"
 fi
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐$NC"
