1cac82bbdc3abed5d8ab896fef630d54f234253a
Lukman Priya <129483274+LawVPN@users.noreply.github.com>
2023-06-07 16:35:05 +0700

    Update menu.sh

--- data/menu.sh
+++ data/menu.sh
@@ -180,7 +180,7 @@ myver="$(cat /opt/.ver)"
 
 if [[ $serverV > $myver ]]; then
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1 $NC ${WH}[${COLOR1}100${WH}]${NC} ${COLOR1}• ${WH}UPDATE TO V$serverV${NC}" 
+echo -e "$COLOR1 $NC ${WH}[${COLOR1}100${WH}]${NC} ${COLOR1}• ${WH}UPDATE TO V$serverV${NC} " 
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 #up2u="updatews"
 up2u="update"
