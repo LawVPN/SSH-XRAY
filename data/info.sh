144ea560d57995c97e798f03fa778d192853ae72
Lukman Priya <129483274+LawVPN@users.noreply.github.com>
2023-06-12 22:04:46 +0700

    Update info.sh

--- data/info.sh
+++ data/info.sh
@@ -20,7 +20,7 @@ cat log-install.txt
 echo -e ""
 echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌────────────────────── ${WH}BY${NC} ${COLOR1}───────────────────────┐${NC}"
-echo -e "$COLOR1 ${NC}                ${WH}• LawNET •${NC}                 $COLOR1 $NC"
+echo -e "$COLOR1 ${NC}                 ${WH}• LawNetwork •${NC}                 $COLOR1 $NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e ""
 read -n 1 -s -r -p "  Press any key to back on menu"
