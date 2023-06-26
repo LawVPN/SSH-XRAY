030c2cf2c1d5fe49a6de6b5bc4c8105ad9aba729
Lukman Priya <129483274+LawVPN@users.noreply.github.com>
2023-06-26 17:53:21 +0700

    Update update.sh

--- data/update.sh
+++ data/update.sh
@@ -7,9 +7,9 @@ echo -e " [INFO] Downloading Update File"
 sleep 2
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/menu.sh" && chmod +x /usr/bin/menu
 wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/menu-ss.sh" && chmod +x /usr/bin/menu-ss
-wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
+#wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
 wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/menu-vless.sh" && chmod +x /usr/bin/menu-vless
-wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
+#wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
 wget -q -O /usr/bin/menu-bot "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/menu-bot.sh" && chmod +x /usr/bin/menu-bot
 wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
 wget -q -O /usr/bin/menu-set "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/menu-set.sh" && chmod +x /usr/bin/menu-set
@@ -26,20 +26,31 @@ wget -q -O /usr/bin/mspeed "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/ma
 wget -q -O /usr/bin/mbandwith "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/menu-bandwith.sh" && chmod +x /usr/bin/mbandwith
 wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/restart.sh" && chmod +x /usr/bin/restart
 wget -q -O /usr/bin/update "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/update.sh" && chmod +x /usr/bin/update
-echo -e " [INFO] Update Successfully"
+#echo -e " [INFO] Update Successfully"
 wget -q -O /opt/.ver "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/version"
 wget -q -O /usr/bin/autoboot "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/autoboot.sh" && chmod +x /usr/bin/autoboot
 wget -q -O /usr/bin/hidessh "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/hidessh" && chmod +x /usr/bin/hidessh
 echo ""
-sleep 2
-echo -e " [INFO] Changelogs on v1.0.5 :
- 1. Fixed minor bugs
- 2. Added HideSSH plugins
- 3. Changed the autoreboot time for every 24 hours from 12 a.m to 5 a.m"
-sleep 2
-echo ""
-echo -e " [INFO] Update UDP SSH soon!"
-echo ""
-sleep 5
-#read -n 1 -s -r -p "Press any key to back on menu";
-#menu
+sleep 2; clear
+
+# 1.0.6
+cd
+wget -q https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/speedtest.sh; chmod +x speedtest.sh && ./speedtest.sh && rm speedtest.sh
+wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
+wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
+
+
+
+echo -e " [INFO] Update Successfully"; sleep 2
+
+echo -e "╔═════════════════════════════════════════════════════════════════════════╗
+║ [INFO] Changelogs on v1.0.6                                             ║
+║   ➢ Fixed speedtest error (now using from the official website)         ║
+║   ➢ Fixed other minor bugs                                              ║
+║ [INFO] Changelogs on v1.0.5 :                                           ║
+║   ➢ Fixed minor bugs                                                    ║
+║   ➢ Added HideSSH plugins                                               ║
+║   ➢ Changed the autoreboot time for every 24 hours from 12 a.m to 5 a.m ║
+╚═════════════════════════════════════════════════════════════════════════╝
+"; sleep 2
+echo "exit in 15 seconds"; sleep 15
