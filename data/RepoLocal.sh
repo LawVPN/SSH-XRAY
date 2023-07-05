fa493cd403a175e2d52598cf23aa43d4d409c3c7
Lukman Priya <129483274+LawVPN@users.noreply.github.com>
2023-07-05 18:22:06 +0700

    Create RepoLocal.sh

---
+++ data/RepoLocal.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo -e "deb http://kartolo.sby.datautama.net.id/debian/ buster main contrib non-free
+deb http://kartolo.sby.datautama.net.id/debian/ buster-updates main contrib non-free
+deb http://kartolo.sby.datautama.net.id/debian-security/ buster/updates main contrib non-free" > /etc/apt/sources.list
