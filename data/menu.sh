289c5e0331f754c149c165ac02a5ac5979dc9af1
Lukman Priya <129483274+LawVPN@users.noreply.github.com>
2023-08-10 02:58:01 +0700

    Update menu.sh

--- data/menu.sh
+++ data/menu.sh
@@ -186,15 +186,15 @@ if [[ $serverV > $myver ]]; then
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
 echo -e "$COLOR1 $NC ${WH}[${COLOR1}100${WH}]${NC} ${COLOR1}• ${RED}UPDATE AUTOSCRIPT TO THE NEWEST ${WH}v$serverV${NC} " 
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-ltsver="${COLOR1}Update Available${WH}』"
+ltsver="『${COLOR1}Update Available${WH}』"
 up2u="updatews"
 else
-ltsver="${COLOR1}Latest${WH}』"
+ltsver="『${COLOR1}Latest${WH}』"
 up2u="menu"
 fi
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐$NC"
 echo -e "$COLOR1 $NC ${WH}Version     ${COLOR1}:${WH} $(cat /opt/.ver) $ltsver${NC}"
-echo -e "$COLOR1 $NC ${WH}Client Name ${COLOR1}: ${WH}T.me/Lawvpn 🇮🇩${NC}"
+echo -e "$COLOR1 $NC ${WH}Client Name ${COLOR1}: ${WH}https://t.me/Lawvpn/ 🇮🇩${NC}"
 echo -e "$COLOR1 $NC ${WH}License     ${COLOR1}: ${WH}Lifetime${NC}"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘$NC"
 echo -e "$COLOR1┌────────────────────── ${WH}BY${NC} ${COLOR1}───────────────────────┐${NC}"
