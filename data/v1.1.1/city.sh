d0d0ed0bd270943d97d8d86bbde69459d2c01c9f
Lukman Priya <129483274+LawVPN@users.noreply.github.com>
2024-01-31 13:01:42 +0700

    Update city.sh

--- data/v1.1.1/city.sh
+++ data/v1.1.1/city.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+IPVPS=$(curl -s ipinfo.io/ip )
 ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
 CITY=$(curl -s ipinfo.io/city )
 
-echo "$ISP" > /etc/lukman/isp
-echo "$CITY" > /etc/lukman/city
+echo "$IPVPS" > /etc/xray/ip
+echo "$ISP" > /etc/xray/isp
+echo "$CITY" > /etc/xray/city
