d41825b6dffde800a558b5fade61399d95a2e0cf
Lukman Priya <129483274+LawVPN@users.noreply.github.com>
2024-03-01 14:38:19 +0700

    Update restore.sh

--- data/v1.1.0/restore.sh
+++ data/v1.1.0/restore.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
-
 clear
-echo "Please make sure you have the link from google drive
-and give access to \"anyone with the link\""
-echo -ne "Link: "; read link
 
-full_link="$link"
-# Menggunakan 'sed' untuk mengekstrak ID dari URL
-id=$(echo "$full_link" | sed -n 's/.*\/d\/\([^\/]*\)\/.*/\1/p')
-echo "Extracted ID: $id"
+echo -n "Restore from gdrive link? [y/N]: "
+read response
 
-newlink="https://drive.google.com/u/4/uc?id=${id}&export=download"
-cd
-wget -q -O backup.zip "$newlink"
+if [ "$response" == "y" ] || [ "$response" == "Y" ]; then
+    echo "Please make sure you have the link from google drive"
+    echo "and give access to \"anyone with the link\""
+    echo -ne "Link: "; read link
+    full_link="$link"
+    
+    # Menggunakan 'sed' untuk mengekstrak ID dari URL
+    id=$(echo "$full_link" | sed -n 's/.*\/d\/\([^\/]*\)\/.*/\1/p')
+    echo "Extracted ID: $id"
 
-echo Start Restore
+    newlink="https://drive.google.com/u/4/uc?id=${id}&export=download"
+    cd
+    wget -q -O backup.zip "$newlink"
+elif [ "$response" == "n" ] || [ "$response" == "N" ]; then
+    # Jika user memilih "n" atau "N", langsung lanjut ke line berikutnya
+    echo "Skipping restore from gdrive."
+    echo "Make sure your backup is right here /root/backup.zip"
+    cd
+else
+    # Jika input tidak valid, kembali ke script sebelumnya
+    echo "Invalid input. Going back."
+    exit 1
+fi
+
+
+
+################echo Start Restore
 unzip backup.zip
 rm -f backup.zip
+################################
 sleep 1
 cd /root/backup
 cp passwd /etc/
