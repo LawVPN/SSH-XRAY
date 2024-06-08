1b75a426624d962cba208a177ea2a6fbe426e41d
Lukman Priya <129483274+LawVPN@users.noreply.github.com>
2024-06-08 19:45:54 +0700

    Update crt.sh

--- data/crt.sh
+++ data/crt.sh
@@ -33,11 +33,42 @@ sleep 2
 echo -e "[ ${green}INFO${NC} ] Processing to stop $Cek " 
 sleep 1
 fi
+echo -e "[ ${green}INFO${NC} ] Cert for domain ipv4/ipv6?[4/6]: "; read ip_version
 echo -e "[ ${green}INFO${NC} ] Starting renew cert... " 
 sleep 2
-/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+#/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+#/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+#~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+
+#read -p "Install certificate for IPv4 or IPv6? (4/6): " ip_version
+#read -p "Enter domain: " domain
+if [[ $ip_version == "4" ]]; then
+    systemctl stop nginx
+    mkdir /root/.acme.sh
+    curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
+    chmod +x /root/.acme.sh/acme.sh
+    /root/.acme.sh/acme.sh --upgrade --auto-upgrade
+    /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+    /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256 --force
+    ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+    echo "Cert installed for IPv4."
+elif [[ $ip_version == "6" ]]; then
+    systemctl stop nginx
+    mkdir /root/.acme.sh
+    curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
+    chmod +x /root/.acme.sh/acme.sh
+    /root/.acme.sh/acme.sh --upgrade --auto-upgrade
+    /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+    /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256 --listen-v6 --force
+    ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+    echo "Cert installed for IPv6."
+else
+    echo "Invalid IP version. Please choose '4' or '6'!"
+    echo -e "[ ${green}INFO${NC} ] Err.. Error detected!"; sleep 1 
+    echo -e "[ ${green}INFO${NC} ] Exiting... "; sleep 2
+    exit 1
+fi
+
 echo -e "[ ${green}INFO${NC} ] Renew cert done... " 
 sleep 2
 echo -e "[ ${green}INFO${NC} ] Starting service $Cek " 
