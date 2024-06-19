b752a3dbc780c6e455145bc95c9ffbcc7164e743
Lukman Priya <129483274+LawVPN@users.noreply.github.com>
2024-06-20 03:40:55 +0700

    Create bwMonitor.sh

---
+++ data/bwMonitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget -q -O /usr/bin/bwMonitor raw.githubusercontent.com/LawVPN/SSH-XRAY/main/data/bwMonitor && chmod +x bwMonitor
+
+cat <<EOF > "/etc/cron.d/bwMonitor"
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 */6 * * * root bwMonitor
+EOF
+
+rm bwMonitor.sh
